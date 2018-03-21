d718e9d5aceea5db6bfab5b0b012dba5d69f6483
twians <35275389+twians@users.noreply.github.com>
2018-03-21 16:22:18 -0500

    Updated 'Pacer' Script
    Pacer now travels the perimeter of the box, rather than traversing one side back and forth.

--- Assets/Scripts/Pacer.cs
+++ Assets/Scripts/Pacer.cs
@@ -6,22 +6,46 @@ public class Pacer : MonoBehaviour {
 	public	float speed		= 5.0f;
 	private	float zMax		= 7.5f;
 	private	float zMin		= -7.5f;
-	private int   direction = 1;
+    private float xMax      = 7.5f;
+    private float xMin      = -7.5f;
+    private int   direction = 1;
+    private int xAxis = 0;
+    private int zAxis = 1;
 
-	void Update() {
-		
-		float zNew = transform.position.z + direction * speed * Time.deltaTime;
-		
-		if (zNew >= zMax) {
-			zNew = zMax;
-			direction *= -1;
-		}
-		else if (zNew <= zMin) {
-			zNew = zMin;
-			direction *= -1;
-		}
-		
-		transform.position = new Vector3(7.5f, 0.75f, zNew);
-	}
-	
+    void Update() {
+
+        float zNew = transform.position.z + direction * speed * Time.deltaTime * zAxis;
+        float xNew = transform.position.x + direction * speed * Time.deltaTime * xAxis;
+
+        if (zNew >= zMax && zAxis == 1)
+        {
+            zNew = zMax;
+            xAxis = 1;
+            zAxis = 0;
+            direction *= -1;
+        }
+        else if (xNew <= xMin && xAxis == 1)
+        {
+            xNew = xMin;
+            xAxis = 0;
+            zAxis = 1;
+        }
+        else if (zNew <= zMin && zAxis == 1)
+        {
+            zNew = zMin;
+            xAxis = 1;
+            zAxis = 0;
+            direction *= -1;
+        }
+        else if (xNew >= xMax && xAxis == 1)
+        {
+            xNew = xMax;
+            xAxis = 0;
+            zAxis = 1;
+        }
+
+        transform.position = new Vector3(xNew, 0.75f, zNew);
+    }
+
 }
+
